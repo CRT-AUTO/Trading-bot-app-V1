2678a6b81040c482f503208797490d73fbc596e4
CRT-AUTO <crtautomations@gmail.com>
2025-04-16 18:43:37 +0700

    Update vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+
+// Add global declarations for our runtime placeholders
+interface Window {
+  __SUPABASE_URL__?: string;
+  __SUPABASE_ANON_KEY__?: string;
+}
